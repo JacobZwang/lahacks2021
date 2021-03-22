46ce2f5c4f328d37b891a614b2292b453935ea5b
JacobZwang <zwang.jacob@gmail.com>
2021-03-21 21:35:58 -0700

    broken when trying to make socket on same port

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,33 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import http from "http";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const httpServer = createServer();
+const app = polka();
+
+app.use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+httpServer.listen(PORT, () => {
+    console.log('socket and sapper running');
+});
+
